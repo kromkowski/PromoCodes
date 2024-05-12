7495fcf0b96169ee3aa9cac02f5e26a23ce26436
Krystian <242510@edu.p.lodz.pl>
2024-05-12 15:23:35 +0200

    Unit test for PromoCodeController Added toJson(), and fromJson methods in PromoCodeMapper

--- src/main/java/com/example/task/mapper/PromoCodeMapper.java
+++ src/main/java/com/example/task/mapper/PromoCodeMapper.java
@@ -2,6 +2,11 @@ package com.example.task.mapper;
 
 import com.example.task.DTO.PromoCodeDTO;
 import com.example.task.model.PromoCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.text.SimpleDateFormat;
 
 public class PromoCodeMapper {
     public static PromoCode toPromoCode(PromoCodeDTO promoCodeDTO ) {
@@ -12,5 +17,28 @@ public class PromoCodeMapper {
                 .maxUsages(promoCodeDTO.getMaxUsages())
                 .expirationDate(promoCodeDTO.getExpirationDate())
                 .build();
+    }
+
+    public static String toJson(PromoCodeDTO promoCodeDTO) {
+        try {
+            return "{\"code\":\"" + promoCodeDTO.getCode() + "\",\"discount\":" + promoCodeDTO.getDiscount() + ",\"currencyCode\":\"" + promoCodeDTO.getCurrencyCode() + "\",\"maxUsages\":" + promoCodeDTO.getMaxUsages() + ",\"expirationDate\":\"" + promoCodeDTO.getExpirationDate() + "\"}";
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting promo code to JSON");
+        }
+    }
+
+    public static PromoCode fromJson(String json) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            objectMapper.setDateFormat(dateFormat);
+
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.readValue(json, PromoCode.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting JSON to promo code");
+        }
     }
 }
